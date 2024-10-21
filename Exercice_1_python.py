c73ec0a9744f5819575c91007f085387b9a400bc
SorelFlora <sorel.f.m.tacune@aims-senegal.org>
2024-10-21 15:48:55 +0000

    Be strong!

---
+++ Exercice_1_python.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+a=int (input ("enter a number A="))
+b=int (input ("enter a number B="))
+c=int (input ("enter a number C="))
+Average=(a+b+c)/3
+Average
+
+
+# In[7]:
+
+
+first_name=str (input("enter first name:"))
+last_name=str (input("enter last name:"))
+print("hello!",first_name, last_name)
+
